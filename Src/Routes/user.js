1f1957b3dff14ac12ab3b5a66701b867c98a2e8a
Parthchauhan19 <chauhanpart6635@gmail.com>
2025-08-08 17:07:56 +0530

    last changes

--- Src/Routes/user.js
+++ Src/Routes/user.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { UserLogin, UserRegistration } from "../Controller/user.js";
 
-
 export const User_Routes = express.Router();
 
 // post routes
 User_Routes.post("/register", UserRegistration);
 User_Routes.post("/login", UserLogin);
+
+//env variables
+// MONGO_URI=mongodb+srv://ParthChauhan:parth198@backend.uebejpl.mongodb.net/OCHI-ADMIN-BACKEND
+// PORT=8000
+// JWT_SECRET="admin123"
